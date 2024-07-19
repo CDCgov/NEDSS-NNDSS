850863a81bb3903732fddbe2cd0810257ee35487
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-07-19 14:23:27 -0400

    setup docker

--- nnd-data-poll-service/src/test/java/gov/cdc/nnddatapollservice/NndDataPollServiceApplicationTests.java
+++ nnd-data-poll-service/src/test/java/gov/cdc/nnddatapollservice/NndDataPollServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package gov.cdc.nnddatapollservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NndDataPollServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package gov.cdc.nnddatapollservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NndDataPollServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
