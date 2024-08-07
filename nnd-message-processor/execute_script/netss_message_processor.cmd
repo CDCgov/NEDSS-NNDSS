8735675e17be9a22937c01dc887cb156df24b102
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-08-07 15:09:48 -0400

    more clean up

--- nnd-message-processor/execute_script/netss_message_processor.cmd
+++ nnd-message-processor/execute_script/netss_message_processor.cmd
@@ -1,5 +1,4 @@
 @echo off
-set NBS_NND_CRON=* * * * * *
 set NND_FILE_LOCATION=/Users/DucNguyen/Desktop/LOG
 set NND_PRIOR=true
 set NND_DATE=12/30/2023
@@ -7,4 +6,4 @@ set OP_DBPASSWORD=fake.fake.fake.1234
 set OP_DBSERVER=localhost:1433
 set OP_DBUSER=sa
 set OP_DBNAME=MSGOUTE
-java -jar nnd-message-processor.jar
+java -jar netss-message-processor.jar
--- nnd-message-processor/execute_script/netss_message_processor.sh
+++ nnd-message-processor/execute_script/netss_message_processor.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export NBS_NND_CRON="* * * * * *"
 export NND_FILE_LOCATION="/Users/DucNguyen/Desktop/LOG"
 export NND_PRIOR="true"
 export NND_DATE="12/30/2023"
@@ -7,4 +6,4 @@ export OP_DBPASSWORD="fake.fake.fake.1234"
 export OP_DBSERVER="localhost:1433"
 export OP_DBUSER="sa"
 export OP_DBNAME="MSGOUTE"
-java -jar nnd-message-processor.jar
+java -jar netss-message-processor.jar
