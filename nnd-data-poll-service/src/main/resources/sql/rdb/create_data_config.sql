75500c98f4ebef17b156774e08529e3acac68e2b
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-08-23 06:58:38 -0400

    Renamed the table.

--- nnd-data-poll-service/src/main/resources/sql/rdb/create_data_config.sql
+++ nnd-data-poll-service/src/main/resources/sql/rdb/create_data_config.sql
@@ -12,13 +12,15 @@ GO
 IF NOT EXISTS(
     SELECT 'X'
     FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'data_exchange_config')
+    WHERE TABLE_NAME = 'poll_data_sync_config')
 BEGIN
-CREATE TABLE data_exchange_config
+CREATE TABLE poll_data_sync_config
 (
     table_name      NVARCHAR(255) NOT NULL PRIMARY KEY,
     source_db       NVARCHAR(255) NOT NULL,
-    last_update_time      DATETIME2
+    last_update_time      DATETIME2,
+    table_order int NULL,
+    query nvarchar(MAX) NOT NULL
 );
 END
 GO
