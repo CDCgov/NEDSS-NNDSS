b3507882ad9e9d8dfec68b71dfe559152cd894af
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-09-18 15:13:44 -0400

    update to handle if incoming is just plain byte[]

--- nnd-data-poll-service/src/main/java/gov/cdc/nnddatapollservice/json_config/Base64Adapter.java
+++ nnd-data-poll-service/src/main/java/gov/cdc/nnddatapollservice/json_config/Base64Adapter.java
@@ -10,12 +10,21 @@ import java.util.Base64;
 public class Base64Adapter extends TypeAdapter<byte[]> {
     @Override
     public void write(JsonWriter out, byte[] value) throws IOException {
-        out.value(Base64.getEncoder().encodeToString(value));
+        if (value != null) {
+            out.value(Base64.getEncoder().encodeToString(value));
+        } else {
+            out.nullValue();  // Handle null case
+        }
     }
 
     @Override
     public byte[] read(JsonReader in) throws IOException {
         String base64 = in.nextString();
-        return Base64.getDecoder().decode(base64);
+        try {
+            return Base64.getDecoder().decode(base64);
+        } catch (IllegalArgumentException e) {
+            // If the value is not properly Base64 encoded, return the byte[] directly
+            return base64.getBytes();
+        }
     }
 }
