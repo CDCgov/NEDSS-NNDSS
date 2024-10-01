d275045f4bd21f1d37c142fd2e89f3f67a80e323
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-10-01 16:24:00 -0400

    added .cmd file

--- nnd-data-poll-service/execute_script/data_sync_rdb_s3.cmd
+++ nnd-data-poll-service/execute_script/data_sync_rdb_s3.cmd
@@ -1,16 +1,22 @@
 @echo off
 set SINGLE_TIME_POLL = false
-set NBS_NND_CRON=0 */1 * * * *
+set NBS_RDB_CRON=0 */1 * * * *
 
-set NND_POLL_ENABLED =true
-set NND_FILE_LOCATION=C:\Users\DucNguyen\Desktop\LOG\Poll
-set NND_FULL_LOAD=false
-set NND_PULL_LIMIT=1000
-set NND_INSERT_LIMIT=1000
 
 set NND_DE_CLIENT_ID=nnd-keycloak-client
 set NND_DE_SECRET=bpVTppDam4sxXt4hfgm5hZ6Rteponjb9
 set NND_DE_URL=http://localhost:8081
+
+set RDB_POLL_ENABLED=true
+set DATASYNC_STORE_S3=true
+set DATASYNC_BATCH_LIMIT=1000
+set DATASYNC_DELETE=false
+
+set AWS_KEY_ID=id
+set AWS_ACCESS_KEY=key
+set AWS_TOKEN=token
+set S3_BUCKET_NAME=bucket
+set S3_REGION=region-2
 
 set OP_DBSERVER=localhost:1433
 set OP_DBNAME=MSGOUTE
--- nnd-data-poll-service/execute_script/data_sync_rdb_sql.cmd
+++ nnd-data-poll-service/execute_script/data_sync_rdb_sql.cmd
@@ -1,16 +1,18 @@
 @echo off
 set SINGLE_TIME_POLL = false
-set NBS_NND_CRON=0 */1 * * * *
+set NBS_RDB_CRON=0 */1 * * * *
 
-set NND_POLL_ENABLED =true
-set NND_FILE_LOCATION=C:\Users\DucNguyen\Desktop\LOG\Poll
-set NND_FULL_LOAD=false
-set NND_PULL_LIMIT=1000
-set NND_INSERT_LIMIT=1000
 
 set NND_DE_CLIENT_ID=nnd-keycloak-client
 set NND_DE_SECRET=bpVTppDam4sxXt4hfgm5hZ6Rteponjb9
 set NND_DE_URL=http://localhost:8081
+
+set RDB_POLL_ENABLED=true
+set DATASYNC_STORE_SQL=true
+set DATASYNC_SQL_ERROR_PATH=/Users/UserName/Desktop/File
+set DATASYNC_BATCH_LIMIT=1000
+set DATASYNC_DELETE=false
+
 
 set OP_DBSERVER=localhost:1433
 set OP_DBNAME=MSGOUTE
