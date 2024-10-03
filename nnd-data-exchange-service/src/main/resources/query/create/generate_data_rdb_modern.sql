ee61a1987b9f9444a1ca145e094960681c14bfe1
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-10-02 21:31:12 -0400

    updated obs coded query

--- nnd-data-exchange-service/src/main/resources/query/create/generate_data_rdb_modern.sql
+++ nnd-data-exchange-service/src/main/resources/query/create/generate_data_rdb_modern.sql
@@ -14,12 +14,11 @@ VALUES
     WHERE RowNum BETWEEN :startRow AND :endRow;')
 ;
 
-
 INSERT INTO [RDB].[dbo].[data_sync_config]
 (table_name, source_db, query, query_with_null_timestamp, query_count, query_with_pagination)
 VALUES
     ('NRT_OBSERVATION_CODED', 'RDB_MODERN',
-    'SELECT *
+    'SELECT rdb_modern.dbo.nrt_observation_coded.*
      FROM rdb_modern.dbo.nrt_observation_coded
      JOIN rdb_modern.dbo.nrt_observation
      ON rdb_modern.dbo.nrt_observation_coded.observation_uid = rdb_modern.dbo.nrt_observation.observation_uid
@@ -35,7 +34,8 @@ VALUES
      OR rdb_modern.dbo.nrt_observation.last_chg_time :operator :timestamp
      OR rdb_modern.dbo.nrt_observation.refresh_datetime :operator :timestamp;',
     'WITH PaginatedResults AS (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum
+        SELECT rdb_modern.dbo.nrt_observation_coded.*,
+               ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum
         FROM rdb_modern.dbo.nrt_observation_coded
         JOIN rdb_modern.dbo.nrt_observation
         ON rdb_modern.dbo.nrt_observation_coded.observation_uid = rdb_modern.dbo.nrt_observation.observation_uid
