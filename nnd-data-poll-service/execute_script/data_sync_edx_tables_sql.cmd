d35717637366ab2cb55e4b3a9330739966c9fa59
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-12-17 10:56:41 -0500

    added new cmd for edx

---
+++ nnd-data-poll-service/execute_script/data_sync_edx_tables_sql.cmd
@@ -0,0 +1,25 @@
+@echo off
+set SINGLE_TIME_POLL = false
+set NBS_RDB_CRON=0 */1 * * * *
+
+
+set NND_DE_CLIENT_ID=nnd-keycloak-client
+set NND_DE_SECRET=bpVTppDam4sxXt4hfgm5hZ6Rteponjb9
+set NND_DE_URL=http://localhost:8081
+
+set RDB_POLL_ENABLED=false
+set EDX_ACTIVITY_ENABLED=true
+set DATASYNC_STORE_SQL=true
+set DATASYNC_SQL_ERROR_PATH=/Users/UserName/Desktop/File
+set DATASYNC_BATCH_LIMIT=1000
+set DATASYNC_DELETE=false
+
+
+set OP_DBSERVER=localhost:1433
+set OP_DBNAME=MSGOUTE
+set RDB_DBNAME=RDB
+set OP_DBPASSWORD=fake.fake.fake.1234
+set OP_DBUSER=sa
+set ODSE_DBNAME=NBS_ODSE;
+
+java -jar data-sync-service.jar
