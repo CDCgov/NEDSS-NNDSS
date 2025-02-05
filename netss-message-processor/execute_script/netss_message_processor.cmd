c818c7b996fadb4a13ae8b07c41d6aac91871ce7
Ragul Shanmugam <rshanmugam@enquizit.com>
2025-02-05 14:21:08 -0800

    Updated arguments for NETSS cmd file

--- netss-message-processor/execute_script/netss_message_processor.cmd
+++ netss-message-processor/execute_script/netss_message_processor.cmd
@@ -1,7 +1,7 @@
 @echo off
-set NND_FILE_LOCATION=/Users/DucNguyen/Desktop/LOG
-set NND_PRIOR=true
-set NND_DATE=12/30/2023
+set NETSS_FILE_LOCATION=/Users/DucNguyen/Desktop/LOG
+set NETSS_PRIOR=true
+set NETSS_DATE=12/30/2023
 set OP_DBPASSWORD=fake.fake.fake.1234
 set OP_DBSERVER=localhost:1433
 set OP_DBUSER=sa
