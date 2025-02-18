41a16f2e1a5c7cf0123b4da24ae61e04fe0a86c5
Duc Nguyen <dnguyen@enquizit.com>
2025-02-18 14:28:36 -0700

    added data log object

---
+++ nnd-data-poll-service/src/main/java/gov/cdc/nnddatapollservice/rdb/dto/PollDataLog.java
@@ -0,0 +1,36 @@
+package gov.cdc.nnddatapollservice.rdb.dto;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+public class PollDataLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private Integer logId;
+
+    @Column(name = "table_name", nullable = false, length = 255)
+    private String tableName;
+
+    @Column(name = "status_sync", nullable = false, length = 20)
+    private String statusSync;
+
+    @Column(name = "start_time", nullable = false)
+    private Timestamp startTime;
+
+    @Column(name = "end_time")
+    private Timestamp endTime;
+
+    @Column(name = "executed_log")
+    private String executedLog;
+
+    @Column(name = "stack_trace")
+    private String stackTrace;
+
+}
