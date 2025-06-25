3a2a5ff3135e7721656dba4868720d521d3c102c
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2025-06-25 12:41:59 -0700

    Cnde 2930 - MT Fixes (#123)
    * add more fine grain loggging
    
    * Added logic to not off setting the Full load
    
    * clean up

--- nnd-data-poll-service/src/main/java/gov/cdc/nnddatapollservice/share/StringUtil.java
+++ nnd-data-poll-service/src/main/java/gov/cdc/nnddatapollservice/share/StringUtil.java
@@ -1,9 +1,17 @@
 package gov.cdc.nnddatapollservice.share;
 
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class StringUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(StringUtil.class);
+
+
     private StringUtil() {
         //SONARQ
     }
@@ -22,6 +30,15 @@ public class StringUtil {
             return false; // No key
         }
         return !keyList.contains(",") && !keyList.contains(" ");
+    }
+
+    public static <T> void printDebugLog(String operation, T data, boolean detailLogApplied) {
+        if (detailLogApplied) {
+            Gson gsonForPrint = new Gson();
+            var str = gsonForPrint.toJson(data);
+            logger.info("{}:\t{}", operation, str);
+        }
+
     }
 
 }
